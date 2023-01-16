2a455b0b4cc26e45b6cb296150a7d080dbff6022
Mle4nik <abdullaev2606@mail.ru>
2023-01-17 01:47:42 +0300

    add image slider

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// import Swiper, { Navigation } from 'swiper';
+
 let call = document.querySelector('.header__call')
 
 // const mainElementWidth = document.documentElement.clientWidth
@@ -49,3 +51,75 @@ window.onload = function () {
     let preload = document.getElementById('preloader')
     preload.style.display = 'none'
 }
+
+
+// инициализируем слайдер
+
+// Swiper.use([Navigation]);
+
+new Swiper('.image-slider', {
+    // стрелки
+    navigation: {
+        nextEL: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true
+    },
+
+    slidesPerView: 3.5,
+
+    spaceBetween: 30,
+
+    // loop: true
+
+    // centeredSlides: true
+
+    autoplay: {
+        delay: 60000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false
+    },
+
+    breakpoints: {
+
+        1699: {
+            slidesPerView: 3.5
+        },
+
+        1399: {
+            slidesPerView: 3,
+            // spaceBetween: 30
+        },
+
+        1023: {
+            slidesPerView: 2.5
+        },
+
+        767: {
+            slidesPerView: 2
+        },
+
+        575: {
+            slidesPerView: 1.8
+        },
+
+        440: {
+            slidesPerView: 1.3
+        },
+
+        320: {
+            slidesPerView: 1
+        },
+
+        0: {
+            slidesPerView: 1
+        },
+    }
+});
